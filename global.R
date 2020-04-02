f4478135dfe2c272fcd8c0e7ea4aa2889fa13cc1
Juuso Korhonen <juuso.korhonen@aalto.fi>
2020-04-02 17:46:16 +0300

    WIP: reading covid data

--- global.R
+++ global.R
@@ -6,7 +6,7 @@ dotenv::load_dot_env('.env')
 
 load_country_names <- function() {
   names_file <- Sys.getenv('COUNTRIES_NAMES_PATH')
-  readr::read_csv(names_file,
+  readr::read_csv(names_file, skip = 1,
                   col_names = c('CNTR_ID', 'CNTR_NAME', 'NAME_ENGL', 'ISO3_CODE'),
                   col_types = c(
                       CNTR_ID = 'c',
@@ -83,15 +83,102 @@ load_countries <- function(file_name) {
   data
 }
 
-read_covid_daily_reports <- function() {
+load_covid_daily_reports <- function() {
   path <- Sys.getenv('COVID_DAILY_REPORTS_PATH')
   
   if (!dir.exists(path)) {
     exit("Path does not exist! Please set COVID_DAILY_REPORTS_PATH.")
   }
   
+  process_file <- function(file_name) {
+    file_path <- paste0(path, "/", file_name)
+    header <- readr::read_lines(file_path, n_max = 1)
+    if (header == "FIPS,Admin2,Province_State,Country_Region,Last_Update,Lat,Long_,Confirmed,Deaths,Recovered,Active,Combined_Key") {
+      # New type  header
+      col_names <- c(
+        "fips",
+        "admin_2",
+        "province_state",
+        "country_region",
+        "last_update",
+        "latitude",
+        "longitude",
+        "confirmed",
+        "deaths",
+        "recovered",
+        "active",
+        "combined_key"
+      )
+      col_types <- readr::cols(
+        fips = readr::col_character(),
+        admin_2 = readr::col_character(),
+        province_state = readr::col_character(),
+        country_region = readr::col_character(),
+        #Last_Update = readr::col_datetime(format = "%Y-%m-%d %H:%M:%S"),
+        last_update = readr::col_character(),
+        latitude = readr::col_double(),
+        longitude = readr::col_double(),
+        confirmed = readr::col_double(),
+        deaths = readr::col_double(),
+        recovered = readr::col_double(),
+        active = readr::col_double(),
+        combined_key = readr::col_character()
+      )
+    } else if (header == "Province/State,Country/Region,Last Update,Confirmed,Deaths,Recovered") {
+       # Old type header
+      col_names <- c(
+        "province_state",
+        "country_region",
+        "last_update",
+        "confirmed",
+        "deaths",
+        "recovered"
+      )
+      col_types <- readr::cols(
+        `province_state` = readr::col_character(),
+        `country_region` = readr::col_character(),
+        #`Last Update` = readr::col_datetime(format = "%m/%d/%Y %H:%M"),
+        `last_update` = readr::col_character(),
+        confirmed = readr::col_double(),
+        deaths = readr::col_double(),
+        recovered = readr::col_double()
+      )
+    } else if (header == "Province/State,Country/Region,Last Update,Confirmed,Deaths,Recovered,Latitude,Longitude") {
+      col_names <- c(
+        "province_state",
+        "country_region",
+        "last_update",
+        "confirmed",
+        "deaths",
+        "recovered",
+        "latitude",
+        "longitude"
+      )
+      col_types <- readr::cols(
+        `province_state` = readr::col_character(),
+        `country_region` = readr::col_character(),
+        #`Last Update` = readr::col_datetime(format = "%m/%d/%Y %H:%M"),
+        `last_update` = readr::col_character(),
+        confirmed = readr::col_double(),
+        deaths = readr::col_double(),
+        recovered = readr::col_double(),
+        latitude = readr::col_double(),
+        longitude = readr::col_double()
+      )
+    } else {
+      print(header)
+      stop("Could not recognize header")
+    }
+    readr::read_csv(file_path, col_names = col_names, col_types = col_types, skip = 1)
+  }
   
-  
+  daily_reports_files <- list.files(path, pattern = ".csv$")
+  daily_reports_files  %>%
+    purrr::map_df(process_file) %>%
+    dplyr::mutate(
+      last_update = lubridate::parse_date_time(last_update, c("mdY HM", "mdy HM", "Ymd HMS")),
+      date = lubridate::as_date(last_update)
+    )
 }
 
 #%% Load values into variables
@@ -99,3 +186,21 @@ read_covid_daily_reports <- function() {
 countries_names <- load_country_names()
 countries_files <- load_country_file_names()
 countries_shapes <- load_countries("/Users/jtkorho2/Developer/beivvi/countries/ref-countries-2016-60m.shp/CNTR_RG_60M_2016_4326.shp.zip")
+
+covid_daily_reports <- load_covid_daily_reports()
+
+covid_daily_reports_country_regions <-
+covid_daily_reports %>%
+  dplyr::distinct(country_region)
+
+covid_daily_reports_by_country_region <-
+covid_daily_reports %>%
+  dplyr::group_by(date, country_region) %>%
+  dplyr::summarise(
+    confirmed = sum(confirmed, na.rm = TRUE),
+    deaths = sum(deaths, na.rm = TRUE),
+    recovered = sum(recovered, na.rm = TRUE)
+  )
+  
+covid_daily_reports_country_regions %>%
+  left_join(by = )
