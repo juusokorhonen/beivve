72fa185e4dbec6bc9ae98794e1f193dcccabcfa3
Juuso Korhonen <juuso.korhonen@aalto.fi>
2020-04-03 17:06:05 +0300

    Added theme and functionality

--- server.R
+++ server.R
@@ -1,22 +1,11 @@
+library(magrittr)
 library(shiny)
 source("./global.R")
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
-  # Get latest values for each country
-  covid_latest_by_country <-
-  covid_daily_reports_by_country_region_daily_changes %>%
-    dplyr::group_by(CNTR_ID) %>%
-    dplyr::arrange(date) %>%
-    dplyr::summarise(
-      confirmed = dplyr::last(confirmed),
-      deaths = dplyr::last(deaths),
-      recovered = dplyr::last(recovered)
-    )
-  
   map_data <-
-  countries_shapes %>%
-    dplyr::left_join(covid_latest_by_country, by = 'CNTR_ID') %>% 
+    covid_daily_reports_by_country_region_daily_changes %>%
     dplyr::group_by(CNTR_ID) %>%
     dplyr::left_join(world_population, by = 'CNTR_ID') %>%
     dplyr::mutate(
@@ -24,62 +13,75 @@ function(input, output, session) {
       f_deaths = 100000 * deaths / population,
       f_recovered = 100000 * recovered / population
     ) %>%
-    dplyr::ungroup()
+    dplyr::ungroup() %>%
+    tidyr::pivot_longer(cols = c('confirmed', 'deaths', 'recovered', 
+                                 'f_confirmed', 'f_deaths', 'f_recovered'),
+                        names_to = 'data_type')
   
-  countries_plot <-
-  map_data %>% 
-    ggplot2::ggplot() +
-    ggplot2::geom_sf(ggplot2::aes(geometry = geometry, fill = f_confirmed), size = 0.25) +
-    ggplot2::coord_sf(crs = sf::st_crs('+proj=robin')) +
-    viridis::scale_fill_viridis(option = "magma", direction = -1) +
-    ggplot2::guides(
-      fill = ggplot2::guide_colorbar(
-        direction = "horizontal",
-        barheight = grid::unit(2, units = "mm"),
-        barwidth = grid::unit(50, units = "mm"),
-        draw.ulim = F,
-        title.position = 'top',
-        # some shifting around
-        title.hjust = 0.5,
-        label.hjust = 0.5
-      )) +
-    theme_map(
-      legend.position = "bottom"
-    )
-   
-    output$map <- renderPlot(
-      {
-        countries_plot
-      })
+  output$map <- renderPlot(
+    {
+      countries_shapes %>%
+        dplyr::left_join(map_data, by = 'CNTR_ID') %>%
+        dplyr::filter(
+          data_type == input$data_type,
+          date <= input$date
+        ) %>%
+        dplyr::arrange(date) %>%
+        dplyr::group_by(CNTR_ID, date, data_type) %>%
+        dplyr::summarise(
+          value = dplyr::last(value)
+        ) %>%
+        dplyr::ungroup() %>%
+        ggplot2::ggplot() +
+        ggplot2::geom_sf(ggplot2::aes(geometry = geometry, fill = value), size = 0.25) +
+        ggplot2::coord_sf(crs = sf::st_crs('+proj=robin')) +
+        viridis::scale_fill_viridis(option = "magma", direction = -1) +
+        ggplot2::guides(
+          fill = ggplot2::guide_colorbar(
+            direction = "horizontal",
+            barheight = grid::unit(2, units = "mm"),
+            barwidth = grid::unit(50, units = "mm"),
+            draw.ulim = F,
+            title.position = 'top',
+            # some shifting around
+            title.hjust = 0.5,
+            label.hjust = 0.5
+          )) +
+        theme_map(
+          legend.position = "bottom"
+        )
+    })
+    
     
     output$info <- renderText({
       paste0("x=", input$map_hover$x, "\ny=", input$map_hover$y)
     })
     
-    #pal <- viridis::viridis_pal(direction = -1, option = 'magma')
-    #pal_func <- leaflet::colorNumeric("viridis", reverse = FALSE, domain = NULL)
-    pal_func <- leaflet::colorBin("magma", reverse = TRUE, domain = NULL, bins = 10)
-    
-    map_data_with_colors <-
-    map_data %>%
-      dplyr::mutate(
-        confirmed_col = pal_func(f_confirmed)
-      ) 
-    
-    output$interactive_map <- leaflet::renderLeaflet({
-      leaflet::leaflet() %>%
-        leaflet::addProviderTiles(leaflet::providers$Stamen.TonerBackground,
-                                  options = leaflet::providerTileOptions(noWrap = TRUE)
-        ) %>%
-        leaflet::addPolygons(data = map_data_with_colors, 
-                  fillColor = ~confirmed_col,
-                  fillOpacity = 0.50,
-                  stroke = TRUE,
-                  color = "#ebebe5",
-                  weight = 1,
-                  layerId = ~CNTR_ID)
-    })
-    
-    map_data_with_colors %>%
-      dplyr::select(confirmed_col)
+    # # Disabled for now
+    # #pal <- viridis::viridis_pal(direction = -1, option = 'magma')
+    # #pal_func <- leaflet::colorNumeric("viridis", reverse = FALSE, domain = NULL)
+    # pal_func <- leaflet::colorBin("magma", reverse = TRUE, domain = NULL, bins = 10)
+    # 
+    # map_data_with_colors <-
+    # map_data %>%
+    #   dplyr::mutate(
+    #     confirmed_col = pal_func(f_confirmed)
+    #   ) 
+    # 
+    # output$interactive_map <- leaflet::renderLeaflet({
+    #   leaflet::leaflet() %>%
+    #     leaflet::addProviderTiles(leaflet::providers$Stamen.TonerBackground,
+    #                               options = leaflet::providerTileOptions(noWrap = TRUE)
+    #     ) %>%
+    #     leaflet::addPolygons(data = map_data_with_colors, 
+    #               fillColor = ~confirmed_col,
+    #               fillOpacity = 0.50,
+    #               stroke = TRUE,
+    #               color = "#ebebe5",
+    #               weight = 1,
+    #               layerId = ~CNTR_ID)
+    # })
+    # 
+    # map_data_with_colors %>%
+    #   dplyr::select(confirmed_col)
 }
--- ui.R
+++ ui.R
@@ -1,13 +1,40 @@
 library(shiny)
+library(shinythemes)
+
+today <- lubridate::now("GMT") %>% lubridate::date()
 
 # Define UI for application that draws a histogram
-fluidPage(
+fluidPage(theme = shinytheme("slate"),
   # Application title
   titlePanel("COVID-19 dashboard"),
-  plotOutput(
-    "map", 
-    hover = "map_hover"),
-  verbatimTextOutput("info"),
-  leaflet::leafletOutput("interactive_map"),
-  p()
+  
+  fluidRow(
+    column(3,
+           wellPanel(
+             sliderInput("date", "Date:",  
+                         min = lubridate::date("2020-01-22"), max = today, value = today,
+                         timeFormat = "%F")
+           ),
+           selectInput("data_type", 
+                       label = "Choose a variable to display",
+                       choices = list("confirmed", 
+                                      "deaths",
+                                      "recovered", 
+                                      "f_confirmed",
+                                      "f_deathrs",
+                                      "f_recovered"),
+                       selected = "confirmed"),
+           
+    ),
+    column(6,
+      plotOutput(
+        "map", 
+        hover = "map_hover"),
+      verbatimTextOutput("info")
+      #leaflet::leafletOutput("interactive_map"),   # Enable on will
+    ),
+    column(3,
+          p("Hello")
+    )
+  )
 )
